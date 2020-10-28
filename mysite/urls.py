50661ef9e606411683ce6993267f9e389ebd8aa8
NgWingKit <wing.ngwingkit@gmail.com>
2020-10-28 12:32:13 +0800

    My Django Girls app, first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
+"""mysite URL Configuration
+
+[...]
+"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
 ]
